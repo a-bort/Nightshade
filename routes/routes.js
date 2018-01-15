715a6c6a79b0abd9843f6a1f2aa7b2355d19f5b8
a-bort <bortron5000@gmail.com>
2018-01-15 09:31:56 -0500

    Serve app with node

---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "", pagename: "index" });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: "", pagename: "index" });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: "About", pagename: "about" });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: "Contact", pagename: "contact" });
+});
+
+router.get('/manifesto', function(req, res, next) {
+  res.render('manifesto', { title: "Manifesto", pagename: "manifesto" });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: "Services", pagename: "services" });
+});
+
+router.get('/values', function(req, res, next) {
+  res.render('values', { title: "Values", pagename: "values" });
+});
+
+module.exports = router;
